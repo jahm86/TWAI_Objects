ddfa832fb7e4f97c9d6c13b2027ea54c57b7259b
jahm86 <jahm86@users.noreply.github.com>
2025-11-01 02:06:55 +0000

    deploy: da042b5c57cd3dce3a8f65a9729440bd53c5edec

--- search/all_1.js
+++ search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['begin_0',['begin',['../classTWAI__Object.html#a1eeef18c8e1371463d56f3537c65bb73',1,'TWAI_Object::begin()'],['../classTWAI__Txcvr.html#a8956e2c9152f1e1296a81c08139471ff',1,'TWAI_Txcvr::begin()']]],
+  ['begin_0',['begin',['../classTWAI__Object.html#a458a195ec32856b8ae417760596a4f1d',1,'TWAI_Object::begin()'],['../classTWAI__Txcvr.html#a8956e2c9152f1e1296a81c08139471ff',1,'TWAI_Txcvr::begin()']]],
   ['browse_20documentation_1',['📚 Browse Documentation',['../index.html#autotoc_md1',1,'']]],
   ['build_20locally_2',['🛠️ Build Locally',['../index.html#autotoc_md3',1,'']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin_0',['begin',['../classTWAI__Object.html#a1eeef18c8e1371463d56f3537c65bb73',1,'TWAI_Object::begin()'],['../classTWAI__Txcvr.html#a8956e2c9152f1e1296a81c08139471ff',1,'TWAI_Txcvr::begin()']]]
+  ['begin_0',['begin',['../classTWAI__Object.html#a458a195ec32856b8ae417760596a4f1d',1,'TWAI_Object::begin()'],['../classTWAI__Txcvr.html#a8956e2c9152f1e1296a81c08139471ff',1,'TWAI_Txcvr::begin()']]]
 ];
